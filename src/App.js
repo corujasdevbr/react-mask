1eb6d9f31b79d0dcb9ed2ae31bd4b75093e00871
corujasdevbr <fernando.guerra@corujasdev.com.br>
2019-11-14 01:05:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { cpfMask, cnpjMask, cepMask, celularMask, telefoneMask } from './mask'
+import { Container, Row, Col, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './App.css';
 
 function App() {
+
+  const [cpf, setCpf] = useState();
+  const [cnpj, setCnpj] = useState();
+  const [cep, setCep] = useState();
+  const [celular, setCelular] = useState();
+  const [telefone, setTelefone] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col className="text-center">
+          <img src="https://www.corujasdev.com.br/wp-content/uploads/2018/09/CorujasDev.png" className="App-logo" alt="logo" />
+        </Col>
+      </Row>
+      <Row>
+        <Col >
+          <p>Mascara de campos de diversos formatos</p>
+          <Form>
+            <Row>
+              <Col>
+                <FormGroup>
+                  <Label for="cpf">CPF</Label>
+                  <Input
+                    maxLength='14'
+                    name='cpf'
+                    value={cpf}
+                    placeholder="___.___.___-__"
+                    onChange={(e) => setCpf(cpfMask(e.target.value))}
+                  />
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup>
+                  <Label for="cnpj">CNPJ</Label>
+                  <Input
+                    maxLength='18'
+                    name='cnpj'
+                    value={cnpj}
+                    placeholder="___.___.___/____-__"
+                    onChange={(e) => setCnpj(cnpjMask(e.target.value))}
+                  />
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup>
+                  <Label for="cep">CEP</Label>
+                  <Input
+                    maxLength='9'
+                    name='cep'
+                    value={cep}
+                    placeholder="_____-___"
+                    onChange={(e) => setCep(cepMask(e.target.value))}
+                  />
+                </FormGroup>
+              </Col>
+
+              <Col>
+                <FormGroup>
+                  <Label for="celular">Celular</Label>
+                  <Input
+                    maxLength='15'
+                    name='celular'
+                    value={celular}
+                    placeholder="(__) _____-____"
+                    onChange={(e) => setCelular(celularMask(e.target.value))}
+                  />
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup>
+                  <Label for="telefone">Telefone</Label>
+                  <Input
+                    maxLength='14'
+                    name='telefone'
+                    value={telefone}
+                    placeholder="(__) ____-____"
+                    onChange={(e) => setTelefone(telefoneMask(e.target.value))}
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container >
   );
 }
 
